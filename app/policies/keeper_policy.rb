730e9482008fe20af42cf38b19cc526902b8c92e
John McIntosh II <jmci10@hotmail.com>
2016-01-23 17:04:28 +0000

    user authorization update

--- app/policies/keeper_policy.rb
+++ app/policies/keeper_policy.rb
@@ -1,2 +1,15 @@
 class KeeperPolicy < ApplicationPolicy
+  
+  def show?
+      user.present? && (record.keep.user == user)
+  end
+  
+  def update?
+    show?
+  end
+  
+  def destroy?
+    show?
+  end
+  
 end
