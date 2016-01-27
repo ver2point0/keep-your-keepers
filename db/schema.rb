1cd348a3ba26dcbafc7d1ea4dd2cdeba348b028a
John McIntosh II <jmci10@hotmail.com>
2016-01-27 09:01:20 +0000

    implement like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102052504) do
+ActiveRecord::Schema.define(version: 20160127090018) do
 
   create_table "keepers", force: :cascade do |t|
     t.string   "url"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20160102052504) do
   end
 
   add_index "keeps", ["user_id"], name: "index_keeps_on_user_id"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "keeper_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["keeper_id"], name: "index_likes_on_keeper_id"
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
