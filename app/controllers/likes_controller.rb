a1adfa847479f8dcd7f9f9022c56988053aeff65
John McIntosh II <jmci10@hotmail.com>
2016-02-02 06:06:42 +0000

    finish like/unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,30 @@
 class LikesController < ApplicationController
   
   def create
+    @keeper = Keeper.find(params[:keeper_id])
+    like = current_user.likes.build(keeper: @keeper)
+    authorize like
     
+    if like.save
+      flash[:notice] = "\"#{@keeper.url}\"  was liked."
+      redirect_to [@keeper.keep, @keeper]
+    else
+      flash[:error] = "\"#{@keeper.url}\"  failed to like."
+      redirect_to [@keeper.keep, @keeper]
+    end
   end
   
-  
   def destroy
+    @keeper = Keeper.find(params[:keeper_id])
+    like = current_user.likes.find(params[:id])
+    authorize like
     
+    if like.destroy
+      flash[:notice] = "\"#{@keeper.url}\"  was unliked."
+      redirect_to [@keeper.keep, @keeper]
+    else
+      flash[:error] = "\"#{@keeper.url}\"  failed to unlike."
+      redirect_to [@keeper.keep, @keeper]
+    end
   end
 end
