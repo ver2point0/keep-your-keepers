fb96a400efc69c3c1a62c538454a65b0a08ab29b
John McIntosh II <jmci10@hotmail.com>
2016-02-13 16:46:09 +0000

    complete user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   
   def show
-    # @user_keepers = 
-    # @liked_keepers = 
+    @user_keepers = current_user.keeps.map(&:keepers)
+    @liked_keepers = current_user.likes.map(&:keeper)
   end
   
 end
